f7cd5a5872d51cc3b9d7e360cd9f578a00d318fc
rajpal229 <rajpalrawat229@gmail.com>
2022-12-19 19:33:51 +0530

    Added: UC1_CreatingStack

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StackAndQueueProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack_Ex stack = new Stack_Ex();
+            Console.WriteLine("Adding Values in Stack using Push");
+            stack.push(70);
+            stack.push(30);
+            stack.push(56);
+            Console.WriteLine("Printing Stack");
+            stack.Display();
+        }
+    }
+}
---
+++ Stack_Ex.cs
@@ -0,0 +1,44 @@
+﻿using StackAndQueueProblems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StackAndQueueProblems
+{
+    internal class Stack_Ex
+    {
+        Node top;
+        public Stack_Ex()
+        {
+            this.top = null;
+
+        }
+        public void push(int data)
+        {
+            Node node = new Node(data);
+            if (top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", data);
+
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
